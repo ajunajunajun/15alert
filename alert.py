27acf4e7287edcd936c606c2440df20eaece4caa
ajun <ajunnajunn@gmail.com>
2019-05-14 14:08:32 +0900

    create crop and import pytesseract

--- alert.py
+++ alert.py
@@ -1,6 +1,13 @@
 # coding: UTF-8
 import pywinauto
+from PIL import Image
+import pytesseract
 
 app = pywinauto.application.Application().connect(title_re=u".*メモ帳.*")
 app_form = app.window(title_re = u".*メモ帳.*")
-app_form.capture_as_image().save('ss.png')
+app_form.capture_as_image().save('screenshot.png')
+
+im = Image.open('screenshot.png')
+im_w, im_h = im.size
+im_crop = im.crop((0, 0, im_w/2, 100))
+im_crop.save('screenshot.png', quality=95)
