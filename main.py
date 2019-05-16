164f24fcbd2f4ca152511d9480975f738b876226
ajun <ajunnajunn@gmail.com>
2019-05-16 14:39:01 +0900

    WIP loop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pywinauto
 from PIL import Image
 import pytesseract
 import sys
+import asyncio
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QThread
@@ -21,27 +22,25 @@ class Window(QWidget):
         btn.clicked.connect(self.buttonClicked)
 
     def buttonClicked(self):
-        while True:
-            app = pywinauto.application.Application().connect(title_re=".*NoxPlayer.*")
-            app_form = app.window(title_re = u".*NoxPlayer.*")
-            app_form.capture_as_image().save('screenshot.png')
+        app = pywinauto.application.Application().connect(title_re=u".*メモ帳.*")
+        app_form = app.window(title_re = u".*メモ帳.*")
+        app_form.capture_as_image().save('screenshot.png')
 
-            im = Image.open('screenshot.png')
-            im_w, im_h = im.size
-            im_crop = im.crop((100, 200, 550, 260))
-            im_crop.save('screenshot.png', quality=95)
+        im = Image.open('screenshot.png')
+        im_w, im_h = im.size
+        im_crop = im.crop((100, 200, 550, 260))
+        im_crop.save('screenshot.png', quality=95)
 
-            im = Image.open('screenshot.png')
-            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-            number = pytesseract.image_to_string(im)
-            print(number)
-            str = number.find('ROUND 15')
-            if str != -1:
-                print('ROUND 15 だよ！！！')
-            QThread.sleep(10)
+        im = Image.open('screenshot.png')
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+        number = pytesseract.image_to_string(im)
+        print(number)
+        str = number.find('ROUND 15')
+        if str != -1:
+            print('ROUND 15 だよ！！！')
+        QThread.sleep(3)
 
 if __name__ == '__main__':
-
     myApp = QApplication(sys.argv)
     myWindow = Window()
     myWindow.show()
