257e3d1f5b6f1fd1ca3cac20fc081836356fd8b3
ajun <ajunnajunn@gmail.com>
2019-05-17 11:37:24 +0900

    create stop button

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pytesseract
 import sys
 import os
 import threading
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import QThread
 from tkinter import messagebox
 
@@ -18,10 +18,26 @@ class Window(QWidget):
         self.setWindowTitle('15Alert')
         self.setWindowIcon(QIcon('hiyoko.png'))
 
-        btn = QPushButton('Button', self)
-        btn.resize(200,40)
+        lbl = QLabel('ROUND15をお知らせするよ\nDota2を起動してからボタン押してね', self)
+        lbl.move(30,20)
+
+        self.img = QLabel(self)
+        pixmap = QPixmap('screenshot.png')
+        self.img.setPixmap(pixmap)
+        self.img.resize(200,40)
+        self.img.move(30,60)
+
+        self.flag = 0
+
+        btn = QPushButton('START', self)
+        btn.resize(100,40)
         btn.move(30,120)
         btn.clicked.connect(self.buttonClicked)
+
+        btn2 = QPushButton('STOP', self)
+        btn2.resize(100,40)
+        btn2.move(130,120)
+        btn2.clicked.connect(self.buttonClicked2)
 
     def alert(self):
         app = pywinauto.application.Application().connect(title_re=u".*メモ帳.*")
@@ -32,21 +48,39 @@ class Window(QWidget):
         im_w, im_h = im.size
         im_crop = im.crop((100, 200, 550, 260))
         im_crop.save('screenshot.png', quality=95)
+        im.close()
 
         im = Image.open('screenshot.png')
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
         number = pytesseract.image_to_string(im)
         print(number)
         str = number.find('ROUND 15')
-        messagebox.showinfo("15Alert","ROUND15だよ！")
         if str != -1:
             print('ROUND 15 だよ！！！')
-        t = threading.Timer(10,self.alert)
-        t.start()
-    def buttonClicked(self):
-        t = threading.Thread(target=self.alert)
-        t.start()
+            messagebox.showinfo("15Alert","ROUND15だよ！")
+        else:
+            if self.flag == 1:
+                t = threading.Timer(5,self.alert)
+                t.start()
+            else:
+                print('stop')
+                self.flag = 0
 
+        pixmap = QPixmap('screenshot.png')
+        self.img.setPixmap(pixmap)
+        im.close()
+
+
+    def buttonClicked(self):
+        if self.flag == 0:
+            print('start')
+            self.flag = 1
+            self.t = threading.Thread(target=self.alert)
+            self.t.start()
+
+    def buttonClicked2(self):
+        if self.flag == 1:
+            self.flag = -1
 
 if __name__ == '__main__':
     myApp = QApplication(sys.argv)
